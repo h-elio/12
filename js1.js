4f08b102bbf23a5379e0e813535e775ba717f426
seonhwan <se0o0ver@gmail.com>
2022-05-18 21:28:43 +0900

    0518

---
+++ js1.js
@@ -0,0 +1,18 @@
+let user = 50;
+let userTelnumber;
+let userAddress;
+
+
+let str = 2022;
+let str2 = '안녕' + str + '\n년';
+
+let str3 = `안녕하세요 올해는 ${str}년
+입니다.`;
+
+let isOn = true;
+let isOff = false;
+
+// console.log(isOn);
+// console.log(isOff);
+
+console.log(typeof userTelnumber);
---
+++ js2.js
@@ -0,0 +1,8 @@
+let user = {
+    name:'kim',
+    age:50,
+    address:'seoul',
+    tel:'01012345678'
+}
+
+console.log(user);
